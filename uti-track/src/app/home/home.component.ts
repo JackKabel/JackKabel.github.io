2c6102d83dbd8e9fd6383e153a2c6f2b9e9ab87d
Abel <antal.abel@utitech.ro>
2025-05-14 16:53:30 +0300

    Start of calendar v.2

--- uti-track/src/app/home/home.component.ts
+++ uti-track/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Client, ID, Storage} from "appwrite";
+import {AuthService} from "../auth/auth.service";
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,19 @@ import {Component} from '@angular/core';
 })
 export class HomeComponent {
 
-  uploadedFile?: File;
+  selectedFile: File | null = null;
+  uploadedFile: File | null = null;
+  private client: Client;
+  private storage: Storage;
+
+  constructor() {
+    this.client = new Client();
+    this.storage = new Storage(this.client);
+
+    this.client
+      .setEndpoint('https://cloud.appwrite.io/v1') // Replace with your Appwrite endpoint
+      .setProject('67935c27000be545cecf'); // Replace with your Appwrite project ID
+  }
 
   uploadFile(event: Event): void {
     event.preventDefault(); // <-- Add this
@@ -17,7 +31,13 @@ export class HomeComponent {
     const file = input.files?.[0];
     if (file) {
       this.uploadedFile = file
-      console.log('Selected file:', file);
+      this.storage.createFile(
+        '679c7995001d789e10e3',
+        ID.unique(),
+        file
+      ).then(r => {
+        console.log('Response:', r);
+      });
     }
   }
 }
